157f15fe4faadca9fc6036b7eec0ff96168a1c79
iamfedoroff <iamfedoroff@gmail.com>
2023-09-26 13:47:10 +0200

    Add inspect3D_poynting_averaged

--- src/plots3D.jl
+++ src/plots3D.jl
@@ -122,6 +122,94 @@ function inspect3D_poynting_xsec(
 end
 
 
+function inspect3D_poynting_averaged(
+    fname; xu=1, yu=1, zu=1, colormap=mak.Reverse(:Hiroshige), colorrange=(0,1),
+    new_window=false, colorbar=true,
+)
+    fp = HDF5.h5open(fname, "r")
+    x = HDF5.read(fp, "x")
+    y = HDF5.read(fp, "y")
+    z = HDF5.read(fp, "z")
+    F = HDF5.read(fp, "Sa")
+    HDF5.close(fp)
+
+    Nx, Ny, Nz = size(F)
+    ix = iseven(Nx) ? div(Nx,2) : div(Nx,2)+1
+    iy = iseven(Ny) ? div(Ny,2) : div(Ny,2)+1
+    iz = iseven(Nz) ? div(Nz,2) : div(Nz,2)+1
+    Lx = x[end] - x[1]
+    Ly = y[end] - y[1]
+    Lz = z[end] - z[1]
+
+    @. x = x / xu
+    @. y = y / yu
+    @. z = z / zu
+
+    @show extrema(F)
+    F .= F ./ maximum(F)
+
+    # --------------------------------------------------------------------------------------
+    fig = mak.Figure(resolution=(950,992), fontsize=14)
+
+    ax = mak.Axis3(
+        fig[1,1]; perspectiveness=0, xlabel="x (μm)", ylabel="y (μm)", zlabel="z (μm)",
+        aspect=(Lx/Lz,Ly/Lz,1),
+    )
+    mak.xlims!(ax, (x[1],x[end]))
+    mak.ylims!(ax, (y[1],y[end]))
+    mak.zlims!(ax, (z[1],z[end]))
+    ax.title[] = @sprintf("%.3f μm  %.3f μm  %.3f μm", x[ix], y[iy], z[iz])
+
+    vol = mak.volumeslices!(
+        ax, x, y, z, F; bbox_visible=false, colormap, colorrange,
+    )
+    vol.update_yz[](ix)
+    vol.update_xz[](iy)
+    vol.update_xy[](iz)
+
+    if colorbar
+        mak.Colorbar(fig[1,2], vol.heatmap_xy[])
+    end
+
+    # --------------------------------------------------------------------------------------
+    sg = mak.SliderGrid(
+        fig[2,1],
+        (label="x", range=1:length(x), startvalue=ix),
+        (label="y", range=1:length(y), startvalue=iy),
+        (label="z", range=1:length(z), startvalue=iz),
+    )
+    mak.on(sg.sliders[1].value) do i
+        ix = i
+        vol.update_yz[](ix)
+        ax.title[] = @sprintf("%.3f μm  %.3f μm  %.3f μm", x[ix], y[iy], z[iz])
+    end
+    mak.on(sg.sliders[2].value) do i
+        iy = i
+        vol.update_xz[](iy)
+        ax.title[] = @sprintf("%.3f μm  %.3f μm  %.3f μm", x[ix], y[iy], z[iz])
+    end
+    mak.on(sg.sliders[3].value) do i
+        iz = i
+        vol.update_xy[](iz)
+        ax.title[] = @sprintf("%.3f μm  %.3f μm  %.3f μm", x[ix], y[iy], z[iz])
+    end
+
+    # hmaps = [vol.heatmap_yz[], vol.heatmap_xz[], vol.heatmap_xy[]]
+    # toggles = [mak.Toggle(sg.layout[i,4], active=true) for i in 1:length(hmaps)]
+    # map(zip(hmaps, toggles)) do (h, t)
+    #     mak.connect!(h.visible, t.active)
+    # end
+
+    if new_window
+        mak.display(mak.Screen(), fig)
+    else
+        mak.display(fig)
+    end
+
+    return nothing
+end
+
+
 function plot3D_poynting_averaged(
     fname;
     xu=1, yu=1, zu=1, norm=false, vmin=0, vmax=1, aspect=:data,
